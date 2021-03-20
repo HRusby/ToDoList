cb58d88cc6068bdee748196b89a0870235f6d894
HRusby <harrygeorger@hotmail.com>
2021-03-20 19:50:19 +0000

    Adding Styling to completed ToDoItem

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -6,7 +6,7 @@ function ToDoItem(props) {
             <input type="checkbox"
                 checked={props.item.completed}
                 onChange={() => props.handleChange(props.item.id)} />
-            <p>{props.item.text}</p>
+            <p className={props.item.completed ? "completedToDoText" : null}>{props.item.text}</p>
         </div>
     )
 }

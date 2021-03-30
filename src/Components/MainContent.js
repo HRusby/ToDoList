a343e4a20f3a240e7542eb53df01c8e7dfb15acc
HRusby <harrygeorger@hotmail.com>
2021-03-30 20:14:19 +0100

    Moving existing logic out of MainContent to a ToDoList component Removing SampleData in favour of API data now that's available

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,63 +1,16 @@
 import React from 'react'
-import ToDoItem from './ToDoItem'
-import ToDoSamples from '../SampleData/ToDoItems'
+import ToDoList from './ToDoList'
 
 class MainContent extends React.Component {
   constructor() {
     super()
-    this.state = {
-      loading: false,
-      todos: ToDoSamples,
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(id) {
-    this.setState((prevState) => {
-      const updatedTodos = prevState.todos.map((todo) => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            isCompleted: !todo.isCompleted,
-          }
-        }
-        return todo
-      })
-      return {
-        todos: updatedTodos,
-      }
-    })
-  }
-
-  componentDidMount() {
-    this.setState((prevState) => {
-      return { ...prevState, loading: true }
-    })
-
-    fetch(
-      'https://localhost:5001/ToDoList/GetAllListItemsForListId',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify('1')
-      })
-      .then(response => response.json())
-      .then(data => this.setState({ loading: false, todos: data }))
-      .then(x => console.log(this.state))
+    this.state = {}
   }
 
   render() {
-    const mainStyles = {
-      margin: 'auto',
-      width: '50%',
-    }
-    const ToDos = this.state.todos.map((item) => (
-      <ToDoItem key={item.id} item={item} handleChange={this.handleChange} />
-    ))
-    const content = this.state.loading ? <p>loading</p> : ToDos
-    return <main style={mainStyles}>{content}</main>
+    return <main>
+      <ToDoList listId='1' />
+    </main>
   }
 }
 
---
+++ src/Components/ToDoList.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react"
+import ToDoItem from './ToDoItem'
+import ToDoSamples from '../SampleData/ToDoItems'
+
+class ToDoList extends Component {
+    constructor(props) {
+        super()
+        console.log(props)
+        this.state = {
+            loading: false,
+            listId: this.props.listId,
+            todos: ToDoSamples,
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map((todo) => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        isCompleted: !todo.isCompleted,
+                    }
+                }
+                return todo
+            })
+            return {
+                ...prevState,
+                todos: updatedTodos,
+            }
+        })
+    }
+
+    componentDidMount() {
+        this.setState(prevState => {
+            return { ...prevState, loading: true }
+        })
+
+        fetch(
+            'https://localhost:5001/ToDoList/GetAllListItemsForListId',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.state.listId)
+            })
+            .then(response => response.json())
+            .then(data => this.setState(prevState => { return { ...prevState, loading: false, todos: data } }))
+            .then(x => console.log(this.state))
+    }
+
+    render() {
+        const mainStyles = {
+            margin: 'auto',
+            width: '50%',
+        }
+        const ToDos = this.state.todos.map((item) => (
+            <ToDoItem key={item.id} item={item} handleChange={this.handleChange} />
+        ))
+        const content = this.state.loading ? <p>loading</p> : ToDos
+        return <div style={mainStyles}>{content}</div>
+    }
+}
+
+export default ToDoList

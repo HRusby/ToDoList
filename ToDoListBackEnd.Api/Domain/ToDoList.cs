dd7d5269267a3ed524399d67d9e491abf3923dd3
HRusby <harrygeorger@hotmail.com>
2021-03-31 18:23:33 +0100

    Removing Items from ToDoList as a ToDoList only serves as a reference to the db not an editor for its items

--- ToDoListBackEnd.Api/Domain/ToDoList.cs
+++ ToDoListBackEnd.Api/Domain/ToDoList.cs
@@ -7,14 +7,6 @@ namespace ToDoListBackEnd.Api.Domain
     {
         public int ListId {get; private set;}
         public string ListName {get; private set;}
-        public IEnumerable<ListItem> Items {get; private set;}
-
-        public ToDoList(int listId, string listName, IEnumerable<ListItem> items)
-        {
-            ListId = listId;
-            ListName = listName;
-            Items = items;
-        }
 
         public ToDoList(int listId, string listName){
             ListId = listId;
